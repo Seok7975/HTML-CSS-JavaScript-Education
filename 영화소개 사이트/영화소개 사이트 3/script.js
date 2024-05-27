0e57cb07c7e2a726a63f83596a827369f17e5905
Park Hong Seok <seok7975@naver.com>
2024-05-27 18:29:48 +0900

    영화소개 사이트 ver3

--- 영화소개 사이트/영화소개 사이트 3/script.js
+++ 영화소개 사이트/영화소개 사이트 3/script.js
@@ -24,26 +24,19 @@
 // });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('introVideo');
-    const container = document.querySelector('.container');
     const videoWrapper = document.querySelector('.video-wrapper');
+    const container = document.querySelector('.container');
+    const nextButton = document.getElementById('nextButton');
 
-    // 비디오 종료 함수
-    function closeVideo() {
-        video.pause(); // 비디오 정지
-        videoWrapper.style.display = 'none'; // 비디오 숨기기
-        container.style.display = 'block'; // 영화 추천 사이트 보이기
-    }
-
-    // ESC 키 이벤트 핸들러
-    document.addEventListener('keydown', function(event) {
-        if (event.key === "Escape") {
-            closeVideo();
-        }
+    nextButton.addEventListener('click', function() {
+        videoWrapper.style.display = 'none';
+        container.style.display = 'block';
     });
 
-    // 마우스 클릭 이벤트 핸들러
-    document.addEventListener('click', function() {
-        closeVideo();
+    document.addEventListener('keydown', function(event) {
+        if (event.key === "Escape") {
+            videoWrapper.style.display = 'none';
+            container.style.display = 'block';
+        }
     });
 });
